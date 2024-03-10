9d54bf9dc04a680fb560e9bf417869f8973755d7
Doruk Demirci <dorukdemirci.boun@gmail.com>
2024-03-10 20:39:32 +0100

    add `assets` alias

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,3 +4,4 @@ require('module-alias/register');
 const moduleAlias = require('module-alias');
 moduleAlias.addAlias('@shared', path.join(__dirname, '../../shared'));
 moduleAlias.addAlias('@entities', path.join(__dirname, './entities'));
+moduleAlias.addAlias('@assets', path.join(__dirname, './assets'));
